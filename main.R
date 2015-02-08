408bc735bc0f600b67aab4460427cddafa83f508
Maxim <maxkrut@users.noreply.github.com>
2015-02-08 22:11:48 +0300

    Update main.R

--- main.R
+++ main.R
@@ -1,6 +1,13 @@
 setClass('_Date')
-setAs("character","_Date", function(from) as.Date(from,"%d/%m/%Y") )
-x <- read.table("household_power_consumption.txt",na.strings='?', header=T, stringsAsFactors=F,sep=";", colClasses=c("_Date","character", rep("numeric",7)))
 
+## setting as %d/%m/%Y format
+setAs("character","_Date", function(from) as.Date(from,"%d/%m/%Y") )
+x <- read.table("household_power_consumption.txt",na.strings='?', 
+                            header=T, stringsAsFactors=F,sep=";", 
+                            colClasses=c("_Date","character", rep("numeric",7)))
+
+## subsetting
 x <- x[x$Date=="2007-02-01"|x$Date=="2007-02-02",]
+
+## merging columns
 times<-strptime(paste(x$Date,x$Time), "%Y-%m-%d %H:%M:%S")
