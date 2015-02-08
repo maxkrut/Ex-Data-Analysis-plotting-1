f8507d3e530fe2f4acfd3daec3cc7f9a6010336c
Maxim <maxkrut@users.noreply.github.com>
2015-02-08 22:12:39 +0300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,6 @@
 source('main.R')
+
 png(filename='output/plot1.png')
 hist(as.numeric(x$Global_active_power), col="red", xlab="Global Active Power (kilowatts)", main ="Global Active Power",bg = "transparent")
+
 dev.off()
