c2215da97a9a13aadd93830796192b875fd893c2
Maxim <maxkrut@users.noreply.github.com>
2015-02-08 22:13:59 +0300

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,4 +1,5 @@
 source('main.R')
+
 png(filename='output/plot3.png')
 plot(times, x$Sub_metering_1, type='l',
      ylab='Energy sub metering',xlab='', bg="transparent")
@@ -6,5 +7,6 @@ lines(times, x$Sub_metering_2, col="red")
 lines(times, x$Sub_metering_3, col="blue")
 legend('topright', legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'),
        col=c('black', 'red', 'blue'),lty='solid')
+       
 dev.off()
 
